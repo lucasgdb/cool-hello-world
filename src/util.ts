91972204f3bae8c1abaed56ef7e64619fec74796
lucasgdb <lucasgdbittencourt@gmail.com>
2022-11-16 16:01:01 -0300

    refactor: improving code

--- src/util.ts
+++ src/util.ts
@@ -11,50 +11,28 @@ export async function write(text: string) {
    let currentTextIndex = 0;
    let currentText = '';
 
+   const consoleWriter = async (characters: string) => {
+      console.log(currentText + characters[currentIndex]);
+
+      await setTimeout(DEFAULT_TIMEOUT);
+
+      if (text[currentTextIndex] === characters[currentIndex]) {
+         currentText += text[currentTextIndex++];
+         currentIndex = 0;
+         return;
+      }
+
+      currentIndex++;
+   };
+
    while (currentText !== text) {
       if (alphabet.includes(text[currentTextIndex])) {
-         console.log(currentText + alphabet[currentIndex]);
-
-         await setTimeout(DEFAULT_TIMEOUT);
-
-         if (text[currentTextIndex] === alphabet[currentIndex]) {
-            currentText += text[currentTextIndex++];
-            currentIndex = 0;
-            continue;
-         }
-
-         currentIndex++;
-      }
-
-      if (ALPHABET.includes(text[currentTextIndex])) {
-         console.log(currentText + ALPHABET[currentIndex]);
-
-         await setTimeout(DEFAULT_TIMEOUT);
-
-         if (text[currentTextIndex] === ALPHABET[currentIndex]) {
-            currentText += text[currentTextIndex++];
-            currentIndex = 0;
-            continue;
-         }
-
-         currentIndex++;
-      }
-
-      if (numbers.includes(text[currentTextIndex])) {
-         console.log(currentText + numbers[currentIndex]);
-
-         await setTimeout(DEFAULT_TIMEOUT);
-
-         if (text[currentTextIndex] === numbers[currentIndex]) {
-            currentText += text[currentTextIndex++];
-            currentIndex = 0;
-            continue;
-         }
-
-         currentIndex++;
-      }
-
-      if (currentIndex === 0) {
+         await consoleWriter(alphabet);
+      } else if (ALPHABET.includes(text[currentTextIndex])) {
+         await consoleWriter(ALPHABET);
+      } else if (numbers.includes(text[currentTextIndex])) {
+         await consoleWriter(numbers);
+      } else if (currentIndex === 0) {
          currentText += text[currentTextIndex++];
 
          console.log(currentText);
