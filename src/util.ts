11e94484f67889d03db9e6d1e36f266562f21f51
lucasgdb <lucasgdbittencourt@gmail.com>
2022-11-16 16:09:06 -0300

    refactor: improving code

--- src/util.ts
+++ src/util.ts
@@ -6,38 +6,48 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 
 export async function write(text: string) {
-   let currentIndex = 0;
-
-   let currentTextIndex = 0;
-   let currentText = '';
+   let currentCharactersIndex = 0,
+      currentTextIndex = 0,
+      currentText = '';
 
    const consoleWriter = async (characters: string) => {
-      console.log(currentText + characters[currentIndex]);
+      console.log(currentText + characters[currentCharactersIndex]);
 
       await setTimeout(DEFAULT_TIMEOUT);
 
-      if (text[currentTextIndex] === characters[currentIndex]) {
+      if (text[currentTextIndex] === characters[currentCharactersIndex]) {
          currentText += text[currentTextIndex++];
-         currentIndex = 0;
+         currentCharactersIndex = 0;
          return;
       }
 
-      currentIndex++;
+      currentCharactersIndex++;
+   };
+
+   const consoleUnspecifiedCharacter = async () => {
+      currentText += text[currentTextIndex++];
+
+      console.log(currentText);
+
+      await setTimeout(DEFAULT_TIMEOUT);
    };
 
    while (currentText !== text) {
       if (alphabet.includes(text[currentTextIndex])) {
          await consoleWriter(alphabet);
-      } else if (ALPHABET.includes(text[currentTextIndex])) {
-         await consoleWriter(ALPHABET);
-      } else if (numbers.includes(text[currentTextIndex])) {
-         await consoleWriter(numbers);
-      } else if (currentIndex === 0) {
-         currentText += text[currentTextIndex++];
-
-         console.log(currentText);
-
-         await setTimeout(DEFAULT_TIMEOUT);
+         continue;
       }
+
+      if (ALPHABET.includes(text[currentTextIndex])) {
+         await consoleWriter(ALPHABET);
+         continue;
+      }
+
+      if (numbers.includes(text[currentTextIndex])) {
+         await consoleWriter(numbers);
+         continue;
+      }
+
+      await consoleUnspecifiedCharacter();
    }
 }
